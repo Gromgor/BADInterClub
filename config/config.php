94e75e5133055c8365e647172f96aea80a7246da
Gromgor <arnaud.agnese@gmail.com>
2016-10-03 16:17:37 +0200

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+	define("SITE_TITRE_PAGES", "Affichage InterClub");
+	define("SITE_AFF_LOGO", false);
+	define("SITE_LOGO", "nice.jpg");
+	define("SITE_NEW_USER", false);
+	define("SITE_APPLI", "Affichage");
+	define("SITE_VERSION", "2.3");
+	define("SITE_PROPRIETAIRE", "MiniGourou");
+	define("LOGO_DEFAULT", "logo-default.jpg");
+?>
---
+++ config/version.php
@@ -0,0 +1,2 @@
+﻿
+		<?php echo SITE_TITRE_PAGES; ?> v.<?php echo SITE_VERSION; ?> &copy; <?php echo SITE_PROPRIETAIRE; ?>
